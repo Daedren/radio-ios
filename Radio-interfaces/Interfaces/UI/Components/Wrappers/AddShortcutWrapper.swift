5126f351692452d46231b123ef744f50f7d7d0f7
Daedren <6081828+Daedren@users.noreply.github.com>
2021-12-25 13:48:35 +0000

    Interaction donation controller now dismissed when buttons tapped

--- Radio-interfaces/Interfaces/UI/Components/Wrappers/AddShortcutWrapper.swift
+++ Radio-interfaces/Interfaces/UI/Components/Wrappers/AddShortcutWrapper.swift
@@ -4,19 +4,32 @@ import IntentsUI
 
 public struct AddShortcutWrapper: UIViewControllerRepresentable {
     var intent: INIntent
+    var delegate: INUIAddVoiceShortcutViewControllerDelegate
     
     public init(intent: INIntent) {
         self.intent = intent
+        self.delegate = Delegate()
     }
 
     public func makeUIViewController(context: UIViewControllerRepresentableContext<AddShortcutWrapper>) -> INUIAddVoiceShortcutViewController {
         if let shortcut = INShortcut(intent: intent) {
             let controller = INUIAddVoiceShortcutViewController(shortcut: shortcut)
+            controller.delegate = self.delegate
             return controller
         }
         fatalError()
     }
 
     public func updateUIViewController(_ uiViewController: INUIAddVoiceShortcutViewController, context: UIViewControllerRepresentableContext<AddShortcutWrapper>) {}
+    
+    private class Delegate: NSObject, INUIAddVoiceShortcutViewControllerDelegate {
+        func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+          controller.dismiss(animated: true, completion: nil)
+        }
+         
+        func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+          controller.dismiss(animated: true, completion: nil)
+        }
+    }
 
 }
